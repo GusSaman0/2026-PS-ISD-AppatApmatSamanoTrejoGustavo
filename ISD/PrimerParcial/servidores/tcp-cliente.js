84de3589931b8692c21a304890e01e1fc8752e73
GusSaman0 <gsamanot2400@alumno.ipn.mx>
2026-09-23 16:28:25 -0600

    tcp cliente y servidor

---
+++ ISD/PrimerParcial/servidores/tcp-cliente.js
@@ -0,0 +1,23 @@
+const net = require('net');
+
+const HOST = process.env.host || '127.0.0.1';
+const PUERTO = proccess.env.PUERTO || 5000;
+
+const socket = net.connect(PUERTO, HOST, () => {
+    console.log(`[TCP] Conectando al HOST ${HOST}:${PUERTO}`);
+    //vamos a enviar un mensaje
+    ['Uno','Dos','Habia una vez','un','patito','Que decia miau miau'].forEach((mensaje) => socket.write(`${mensaje}/n`));
+    socket.end;
+});
+
+socket.on('data', (datos) => {
+    process.stdout.write(`[TCP] ${datos.toString()}`);
+});
+
+socket.on('close', () => {
+    console.log(`[TCP] Conexión Cerrada con el Servidor`);
+});
+
+socket.on('error', (error) => {
+    console.log(`[TCP] No se pudo conectar`, error.message);
+});
---
+++ ISD/PrimerParcial/servidores/tcp-servidores.js
@@ -0,0 +1,32 @@
+const net = require('net');
+
+const PUERTO = proccess.env.PUERTO || 5000;
+
+const servidor = net.createServer((socket)=>{
+    const cliente = `${socket.remoteAdresss}:${socket.remotePort}`;
+    //Aqui el evento de conexion solo se dispara en el momento en qe cliente y servidor establecen un thre-way a travez del handshake (el apreton de manos es el momento en que el cliente realiza una peticíon , envia IP,Puerto, Datagrama y saluda al servidor, el servidor le responde creando la sesión)
+    console.log(`[TCP] Conexión Establecida con el Cliente ${cliente}`);
+    socket.on('data',(datos) => {
+        const crudo = datos.toString();
+        //TCP es un flujo de bytes entonces nosotros en el socket Lo vamos a transformar en cadenas
+        console.log(`[TCP] Datos crudos asi crudisimos: ${datos.length} bytes: ${JSON.stringify(crudo)}`);
+
+        const lineas = crudo.split('\n').map((l) => l.trim()).filter(Boolean);
+        lineas.forEach((linea) => {
+            console.log(`[TCP] Mensaje: "${linea}"`);
+            socket.write(`Eco TCP: ${linea}\n`);
+        });
+    });
+});
+
+socket.on('close', () => {
+    console.log(`[TCP] Conexión Cerrada con el cliente: ${cliente}`);
+});
+
+socket.on('error', (error) => {
+    console.log(`[TCP] Error con: ${cliente}`, error.message);
+});
+
+servidor.listen(PUERTO, () => {
+    console.log(`Servidor inicializado en: ${PUERTO}`);
+});
